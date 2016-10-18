ad86bd934a022f158e14b534f16e46329a1a480b
Alfred Garcia <alfred.garcia@illumio.com>
2016-10-18 15:58:47 -0700

    add repr() to deal with blank value returns

--- aws_list.py
+++ aws_list.py
@@ -17,4 +17,4 @@ for i in range(0,len(regions)):
     ec2 = boto3.resource('ec2', region_name=regions[i])
     instances = ec2.instances.filter(Filters=[{'Name': 'instance-state-name', 'Values': ['running']}])
     for instance in instances:
-        print instance.id+",", instance.instance_type+",", instance.public_ip_address+",", instance.public_dns_name
+        print repr(instance.id)+",", repr(instance.instance_type)+",", repr(instance.public_ip_address)+",", repr(instance.public_dns_name)
